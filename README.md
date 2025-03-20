9600f01a3ec25777c4c6e6240ed652940b92c088
coriamartinn <coria10m@gmail.com>
2025-03-20 18:13:06 -0300

    Agregado de database y ajustes finales

---
+++ README.md
@@ -0,0 +1,15 @@
+# CRUD Flask Project
+
+Un CRUD básico con Flask y MySQL.
+
+## Instalación
+
+1. Clonar el repositorio:
+2. Crear entorno virtual e instalar dependencias:
+3. Configurar la base de datos:
+
+- Crear una base de datos en MySQL.
+- Ejecutar `database.sql` para generar las tablas.
+- Configurar la conexión en `app.py`.
+
+4. Ejecutar la aplicación:
---
+++ crud_tareas.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-03-2025 a las 22:09:51
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `crud_tareas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tareas`
+--
+
+CREATE TABLE `tareas` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `descripcion` text NOT NULL,
+  `estado` enum('pendiente','en progreso','completada') NOT NULL DEFAULT 'pendiente',
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tareas`
+--
+
+INSERT INTO `tareas` (`id`, `titulo`, `descripcion`, `estado`, `fecha_creacion`) VALUES
+(7, 'EXAMEN DE MATEMATICAS', 'PRIMER PARCIAL INECUACIONES Y FUNCIONES', 'pendiente', '2025-03-20 17:01:51'),
+(8, 'COMPRAS', 'POLLO, TAPITA DE ASADO, ZAPALLITOS, TOMATES, MANZANAS VERDES', 'pendiente', '2025-03-20 17:02:44'),
+(10, 'TOBY', 'ASDJAWSFJAWRF', 'completada', '2025-03-20 17:50:28');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
